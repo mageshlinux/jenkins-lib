a97e7318cda4546bf9f7ef75ceba1b3fc5a14ea6
mageshlinux <35847347+mageshlinux@users.noreply.github.com>
2021-07-01 01:14:27 -0700

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,12 +1,3 @@
-def call(args){
-    pipeline {
-        agent any 
-        stages { 
-            stage('checkout') {
-                steps {
-                    git branch: 'dev', credentialsId: 'github-cred', url: 'https://github.com/mageshlinux/magesh-test.git'
-                }
-            }
-	}	
-    }
+def call(int a){
+   echo a
 }
