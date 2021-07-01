335dd6f0d54346734ab5be6a25b7a2ea4bd93830
mageshlinux <35847347+mageshlinux@users.noreply.github.com>
2021-07-01 11:27:38 -0700

    Update gitflowstart.groovy

--- vars/gitflowstart.groovy
+++ vars/gitflowstart.groovy
@@ -4,8 +4,8 @@ pipeline{
     stages{
         stage("Env Build Number"){
             steps{
-                sh 'echo "The build number is ${env.BUILD_NUMBER}"'
-                sh 'echo "You can also use \${BUILD_NUMBER} -> ${BUILD_NUMBER}"'
+                println ${env.BUILD_NUMBER}
+                
             }
         }
     }
