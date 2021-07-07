7bf17e54e1a5fe4a9557e55902b11259f590b8dc
mageshlinux <35847347+mageshlinux@users.noreply.github.com>
2021-07-07 20:58:00 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,8 +1,4 @@
 def call(args) {
-node {
-    stage('Example') {
-      checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'github-cred', url: 'https://github.com/mageshlinux/magesh-test.git']]])
-    }
- }
 
+  checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'github-cred', url: 'https://github.com/mageshlinux/magesh-test.git']]])
 }
